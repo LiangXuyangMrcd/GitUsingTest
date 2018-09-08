3bc0de0b47783b5aa4d8f42b42c8b59a47625e9f
LiangYang88 <zhumenggaofei@gmail.com>
2018-09-08 12:30:35 +0800

    分支测试

--- app/src/main/java/com/example/administrator/gitusingtest/MainActivity.java
+++ app/src/main/java/com/example/administrator/gitusingtest/MainActivity.java
@@ -13,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
         TextView text = findViewById(R.id.text);
         text.setText("哈哈哈");
+
+        String newContent = "";
+        newContent = "123456";
+        text.setText(newContent);
     }
 }
