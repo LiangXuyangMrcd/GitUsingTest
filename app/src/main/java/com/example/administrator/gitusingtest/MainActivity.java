a01e88536bc50def6a4083f988c9689855ac41d5
LiangXuyang <Liangxuyang@hinterstellar.com>
2018-09-09 15:02:47 +0800

    master第三次提交

--- app/src/main/java/com/example/administrator/gitusingtest/MainActivity.java
+++ app/src/main/java/com/example/administrator/gitusingtest/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 text.setText("");
                 //1
                 //2
+                //3
 
             }
         });
