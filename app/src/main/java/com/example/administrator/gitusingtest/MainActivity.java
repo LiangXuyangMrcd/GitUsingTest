7f41b68cdbbc815cc498ae493d778e824396a8fd
LiangXuyang <Liangxuyang@hinterstellar.com>
2018-09-09 15:03:47 +0800

    test 第二次提交

--- app/src/main/java/com/example/administrator/gitusingtest/MainActivity.java
+++ app/src/main/java/com/example/administrator/gitusingtest/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
                  * 我在这里合并冲突
                  */
                 //1
+                //2
                 text.setText("");
 
             }
