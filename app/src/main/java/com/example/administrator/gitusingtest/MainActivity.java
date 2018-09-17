3b9eb3aa75aae2b6d5cbdddca1b93f4f2951ce4d
LiangXuyang <Liangxuyang@hinterstellar.com>
2018-09-18 02:08:35 +0800

    %decaltion%

--- app/src/main/java/com/example/administrator/gitusingtest/MainActivity.java
+++ app/src/main/java/com/example/administrator/gitusingtest/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
                 /**
                  * MarkDown 测试
                  *
-                 * 
+                 *
                  */
                 text.setText("");
 
